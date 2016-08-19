41681baff288a99e050dd8b331ce9423ecc3dd9d
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-19 18:56:21 -0500

    Basic functions, not sure if it is working due to "undefined" in console.log. Updated the psuedocode

--- public/s.simon.js
+++ public/s.simon.js
@@ -2,39 +2,63 @@
 	"use strict";
 
 // ==================== VARIABLES ====================
+	var playerOrder = []
+	var gameOrder = []
+	var score = 0
+
+
 // ==================== RANDOM SELECTOR ====================
-	function getRandom() {
-		// generates random number between 1 and 4
-		var num = Math.floor(Math.random() * 4) + 1;
-
-		// takes generated number and turns it into a color
-		if (num == 1) {
-			num = "green";
-			// num = $("#box1");
-		} if (num == 2) {
-			num = "red";
-			// num = $("box2");
-		} if (num == 3) {
-			num = "blue";
-			// num = $("box3");
-		} if (num == 4) {
-			num = "yellow";
-			// num = $("box4");
-		};
-		console.log(num) 
+	function getRandom(turns) {
+		for (var i = 0; i < turns; i++) {
+			this.gameOrder.push(Math.floor(Math.random() * 4) + 1);
+		}
 	};
-
-// ====================  ====================
 
 // ==================== LIGHTS BOX ====================
-	function lightBox() {
-		var order = [];
+	function lightBox(element, level, speed) {
+		if (level > 0) {
+			element.stop().animate({opacity: "1"}, {
+				duration: 50, function() {
+					element.stop().animate({opacity: "0.5"}, 200);
+				}
+			});
+		}
+		if (level > 0) {
+			setTimeout(function(){
+				this.lightBox(element, time, speed)
+			}, speed);
+			time -= 1;
+		}
 	};
-// ====================  ====================
-// ====================  ====================
-// ====================  ====================
-// ====================  ====================
 
+// ==================== GAME ORDER ====================
+	function flashOrder() {
+		$.each(gameOrder, function(index, value) {
+			setTimeout(function() {
+				this.lightBox($(this.shape + value), 1, 300, value);
+			}, 500)
+		});
+	};
+
+// ==================== SCORE ====================
+	function playerScore() {
+		$("#winCounts").text(score)
+	}
+// ====================  ====================
+// ====================  ====================
 getRandom();
 
+
+
+
 // })();
+
+
+
+
+
+
+
+
+
+
