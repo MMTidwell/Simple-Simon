92f5a56855a50d74ef0ed8dc9066270a4e481382
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-20 12:06:59 -0500

    Working with Psudeocode and rethinking process

--- public/s.simon.js
+++ public/s.simon.js
@@ -1,60 +1,62 @@
-// (function(){
-	"use strict";
 
-// ==================== VARIABLES ====================
-	var playerOrder = []
-	var gameOrder = []
-	var score = 0
+// // ==================== VARIABLES ====================
+// 	var playerOrder = []
+// 	var gameOrder = []
+// 	var score = 0
 
 
-// ==================== RANDOM SELECTOR ====================
-	function getRandom(turns) {
-		for (var i = 0; i < turns; i++) {
-			this.gameOrder.push(Math.floor(Math.random() * 4) + 1);
-			lightBox()
-		}
-	};
+// // ==================== RANDOM SELECTOR ====================
+// 	function getRandom(turns) {
+// 		for (var i = 0; i < turns; i++) {
+// 			this.gameOrder.push(Math.floor(Math.random() * 4) + 1);
+// 			lightBox()
+// 		}
+// 	};
 
-// ==================== LIGHTS BOX ====================
-	function lightBox(element, level, speed) {
-		if (level > 0) {
-			element.stop().animate({opacity: "1"}, {
-				duration: 50, function() {
-					element.stop().animate({opacity: "0.5"}, 200);
-				}
-			});
-		}
-		if (level > 0) {
-			setTimeout(function(){
-				this.lightBox(element, time, speed)
-			}, speed);
-			time -= 1;
-		}
-	};
+// // ==================== LIGHTS BOX ====================
+// 	function lightBox(element, level, speed) {
+// 		if (level > 0) {
+// 			element.stop().animate({opacity: "1"}, {
+// 				duration: 50, function() {
+// 					element.stop().animate({opacity: "0.5"}, 200);
+// 				}
+// 			});
+// 		}
+// 		if (level > 0) {
+// 			setTimeout(function(){
+// 				this.lightBox(element, time, speed)
+// 			}, speed);
+// 			time -= 1;
+// 		}
+// 	};
 
-// ==================== GAME ORDER ====================
-	function flashOrder() {
-		$.each(gameOrder, function(index, value) {
-			setTimeout(function() {
-				this.lightBox($(this.shape + value), 1, 300, value);
-			}, 500)
-		});
-	};
+// // ==================== GAME ORDER ====================
+// 	function flashOrder() {
+// 		$.each(gameOrder, function(index, value) {
+// 			setTimeout(function() {
+// 				this.lightBox($(this.shape + value), 1, 300, value);
+// 			}, 500)
+// 		});
+// 	};
 
-// ==================== SCORE ====================
-	function playerScore() {
-		$("#winCounts").text(score)
-	}
+// // ==================== SCORE ====================
+// 	function playerScore() {
+// 		$("#winCounts").text(score)
+// 	}
+// // ==================== SCORE ====================
+
+
+
+
+// $(document).ready(function(){
+
+// });
+
 // ====================  ====================
 // ====================  ====================
-
-
-
-
-
-// })();
-
-
+// ====================  ====================
+// ====================  ====================
+// ====================  ====================
 
 
 
