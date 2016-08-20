c8e8f7b7f7f1b1ad8927a50ffb35e4dc1e7e18c9
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-20 18:19:37 -0500

    Got flash woking in array order

--- public/s.simon.js
+++ public/s.simon.js
@@ -1,34 +1,8 @@
 
-// // ==================== VARIABLES ====================
-// 	var playerOrder = []
-// 	var gameOrder = []
-// 	var score = 0
 
 
-// // ==================== RANDOM SELECTOR ====================
-// 	function getRandom(turns) {
-// 		for (var i = 0; i < turns; i++) {
-// 			this.gameOrder.push(Math.floor(Math.random() * 4) + 1);
-// 			lightBox()
-// 		}
-// 	};
 
-// // ==================== LIGHTS BOX ====================
-// 	function lightBox(element, level, speed) {
-// 		if (level > 0) {
-// 			element.stop().animate({opacity: "1"}, {
-// 				duration: 50, function() {
-// 					element.stop().animate({opacity: "0.5"}, 200);
-// 				}
-// 			});
-// 		}
-// 		if (level > 0) {
-// 			setTimeout(function(){
-// 				this.lightBox(element, time, speed)
-// 			}, speed);
-// 			time -= 1;
-// 		}
-// 	};
+
 
 // // ==================== GAME ORDER ====================
 // 	function flashOrder() {
@@ -56,43 +30,52 @@
 	var clientArray = [];
 	var gameArray = [];
 
+
 // ==================== START BUTTON ====================
 	$("#playGame").click(function() {
 		game();
 	});
 
+	
 // ==================== GAME ====================
 	function game() {
 		// pulls random number between 1 and 4 and keeps an array
-		gameArray = gameArray.push(Math.floor(Math.random() * 4) + 1);
-		console.log(gameArray);
+		gameArray.push(Math.floor(Math.random() * 4) + 1);
+		// console.log(gameArray + " game()");
 
 	// ==================== LIGHTS UP ====================
-		switch (gameArray) {
-			case 1:
-				lightUp("#green");
-				// console.log("green")
-				break;
-			case 2:
-				lightUp("#red");
-				// console.log("red")
-				break;
-			case 3:
-				lightUp("#blue");
-				// console.log("blue")
-				break;
-			case 4:
-				lightUp("#yellow");
-				// console.log("yellow")
-				break;
+		// let declares i for each iteration, not the loop. This way what is passed to the setTimeout is exactly what we want. let is scoped for the loop, var is scoped to the function. 
+		for (let i = 0; i < gameArray.length; i++) {
+			setTimeout(function() {
+				switch (gameArray[i]) {
+					case 1:
+						lightUp("#green");
+						// console.log("success1")
+						break;
+					case 2:
+						lightUp("#red");
+						// console.log("success2")
+						break;
+					case 3:
+						lightUp("#blue");
+						// console.log("success3")
+						break;
+					case 4:
+						lightUp("#yellow");
+						// console.log("success4")
+						break;
+				}
+			}, i * 600);
 		}
 
 		function lightUp(box) {
 			$(box).fadeTo("fast", 1).fadeTo("fast", 0.5);
+
+			console.log(gameArray + " lightUp()");
+			console.log("\n")
 		}
 		
-
-	};	
+	}
 
 // ==================== CLIENT BUTTON ====================
 // ==================== COMPARE ARRAYS ====================
