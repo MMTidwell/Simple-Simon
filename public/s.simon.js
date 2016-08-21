44d9849b3d81d857b2c61ff667128f51e253a2df
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-20 22:29:13 -0500

    Made SS functional, cleaned up and added comments

--- public/s.simon.js
+++ public/s.simon.js
@@ -1,82 +1,86 @@
-// (function(){ 
+(function(){ 
 
-// ==================== VARIABLES ====================
+	// ==================== VARIABLES ====================
 	var clientArray = [];
 	var gameArray = [];
+	var level = 0
 
-// ==================== START BUTTON ====================
+	// ==================== START BUTTON ====================
+	// starts game once the play button is clicked
 	$("#playGame").click(function() {
 		gameOrder();
 	});
 
-// ==================== GAME ====================
+	// ==================== GAME ORDER ====================
 	function gameOrder() {
 		// pulls random number between 1 and 4 and keeps an array
 		gameArray.push(Math.floor(Math.random() * 4) + 1);
-		// console.log(gameArray + " game()");
 
-		// ==================== LIGHTS UP ====================
 		// let declares i for each iteration, not the loop. This way what is passed to the setTimeout is exactly what we want. let is scoped for the loop, var is scoped to the function. 
 		for (let i = 0; i < gameArray.length; i++) {
+			// initiate the delay between index of gameArray
 			setTimeout(function() {
 				switch (gameArray[i]) {
 					case 1:
 						lightUp("#green");
-						// console.log("success1")
 						break;
 					case 2:
 						lightUp("#red");
-						// console.log("success2")
 						break;
 					case 3:
 						lightUp("#blue");
-						// console.log("success3")
 						break;
 					case 4:
 						lightUp("#yellow");
-						// console.log("success4")
 						break;
 				}
-			}, i * 600);
+			// i represents the delay used after the button is added to the array
+			}, i * 700);
 		}
-
-		function lightUp(box) {
-			$(box).fadeTo("fast", 1).fadeTo("fast", 0.5);
-			// console.log(gameArray + " lightUp()");
-		}	
 	} 
 
+	// ==================== LIGHTS UP ====================
+	// lights up the selected box
+	function lightUp(box) {
+		$(box).fadeTo("fast", 1).fadeTo("fast", 0.5);
+	}	
+
 	// ==================== CLIENT CLICKS ====================
+	// makes the boxes clickable and sends them lightUp
 	$(".boxed").click(function() {
+		var lightOnClick = "#" + $(this).attr("id");
+		lightUp(lightOnClick);
+		// adds the click to array as a number
 		clientArray.push(parseInt($(this).attr("class").split(' ')[1]));
-		// console.log(clientArray[0]);
 
-		if (clientArray == gameArray)
-
-		compareArrays();
-
+		// checks to see if the lengths are the same for both arrays
+		if (clientArray.length == gameArray.length) {
+			compareArrays();
+		} 
 	});
 
-// ==================== COMPARE ARRAYS ====================
+	// ==================== COMPARE ARRAYS ====================
+	// compares arrays
 	function compareArrays() {
 		for (var i = 0; i < gameArray.length; i++) {
-			console.log("gameArray() " + gameArray);
-			console.log("clientArray() " + clientArray);
-
-			if (gameArray[i] == clientArray[i]) {
-				clientArray = [];
-			} else {
-				alert("You have died a miserable death \nand my little puppy can do better than you!")
+			if (gameArray[i] != clientArray[i]) {
+				alert("You have died a miserable death \nand my little puppy can do better than you!");
 				break;
 			}
 		}
-		gameOrder();
+		// clears client array so they can play the next level
+		clientArray = [];
+		level++
+		// updates the score 
+		$('#winCounts').html(level);
+
+		// sets a delay between client clicks and game clicks
+		setTimeout(function() {
+			gameOrder();
+		}, 500);
 	}
 
-
-// ==================== UPDATE SCORE ====================
-
-// })();
+})();
 
 
 
